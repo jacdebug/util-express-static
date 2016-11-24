6a7139acb140b022c73a58ac6a208b3a2fb4fe19
Deepak Jacob <jacob.deepak@yahoo.com>
2016-11-24 14:26:21 +0000

    Inital commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('express up')
+});
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+});
