e31e8c483d321030653f9fcb233b8719349f4173
Deepak Jacob <jacob.deepak@yahoo.com>
2016-11-25 11:03:28 +0000

    Just whitespace changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('express up')
+  res.send('express up')
 });
 
 app.use(express.static('public'));
