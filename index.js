e0f76839d504d5ef54433bd2eca94d2b2cda4845
Deepak Jacob <jacob.deepak@yahoo.com>
2016-12-03 07:14:34 +0000

    open url once server is running

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const opn = require('opn');
+
 const app = express();
+const PORT = 3000;
+const INDEXURL = `http://localhost:${PORT}/index.html`;
 
 app.get('/', (req, res) => {
-  res.send('express up')
+  res.send('This is root path, please type filename full path to url')
 });
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('Static server listening on port 3000, serviing files from public folder.')
+app.listen(PORT, () => {
+  console.log(`util-static-server running on ${INDEXURL}`);
+  opn(INDEXURL)
 });
