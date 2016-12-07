f5e8257958f0fd323bb5800667907bd085f7d2b7
Deepak Jacob <jacob.deepak@yahoo.com>
2016-12-07 20:56:04 +0000

    stop opening window while running test

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require('express');
 const opn = require('opn');
 
 const app = express();
+const istest = process.env.NODE_ENV === 'test';
+
 const PORT = 3000;
 const INDEXURL = `http://localhost:${PORT}/index.html`;
 
 app.get('/', (req, res) => {
-  res.send('This is root path, please type filename full path to url')
+  res.send('This is root path, please type filename full path to url');
 });
 
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`util-static-server running on ${INDEXURL}`);
-  opn(INDEXURL)
+  if(!istest) opn(INDEXURL);
 });
 
 module.exports = app;
